cebd3dbfb6fd129a7b0acb05af04873f7495bfa0
Michael Whyte <info@mwhyte.ca>
2018-07-12 11:33:09 -0700

    Create animal words array

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,77 @@
 // Hangman Game
 // Ver: 1.0.0
+
+(function($){
+
+"use strict";
+
+// Game Class
+class Game {
+
+    constructor(){
+        this.words = [
+            'reindeer',
+            'wolverine',
+            'gorilla',
+            'giraffe',
+            'salamander',
+            'rabbit',
+            'alligator',
+            'kangaroo',
+            'beaver',
+            'hedgehog',
+            'leopard',
+            'cheetah',
+            'turtle',
+            'porcupine',
+            'baboon',
+            'elephant',
+            'antelope',
+            'raccoon',
+            'koala',
+            'panda',
+            'coyote',
+            'squirrel',
+            'baboon',
+            'tiger',
+            'hippopotamus',
+            'chameleon',
+            'warthog',
+            'moose',
+            'chipmunk',
+            'hyena',
+            'badger',
+            'buffalo',
+            'skunk',
+            'orangutan',
+            'anteater',
+            'rhinoceros',
+            'alpaca',
+            'gazelle',
+            'lemur',
+            'jackal',
+            'chimpanzee',
+            'weasel',
+            'gopher',
+            'wolverine',
+            'zebra',
+            'meerkat'
+        ]
+    }
+
+}
+
+// Game DOM Elements
+const $btnStart = $('#btn-start');
+const $gameForm = $('#form-game');
+
+// Create a new instance of the Game
+// Class
+const game = new Game();
+
+console.log(game.words);
+console.log(game.words.length);
+
+// Game Event Handlers
+
+})(jQuery);
